e8febce2352468124e3e597f6e2e727dca10a862
Uiriamuzu <daviswill264@gmail.com>
2026-03-25 15:54:42 +0100

    deployed successfully on testnet aggron

--- scripts/check_lumos.js
+++
@@ -1,4 +0,0 @@
-const lumos = require("@ckb-lumos/lumos");
-console.log("Keys in @ckb-lumos/lumos:", Object.keys(lumos));
-if (lumos.commons) console.log("Keys in commons:", Object.keys(lumos.commons));
-if (lumos.helpers) console.log("Keys in helpers:", Object.keys(lumos.helpers));
